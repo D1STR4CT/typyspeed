567c9bc232f7af677f7959f2eff2d241dc329252
D1STR4CT <slmkuitert@gmail.com>
2021-05-06 17:07:41 +0200

    beta 0.1.0

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from pynput.keyboard import Key, Listener
 from time import time, sleep
+import os 
+import linecache
 
 
 def add_to_log(log): 
@@ -7,27 +9,55 @@ def add_to_log(log):
         f.write(log)
 
 def on_press(key):
-    log = f"{key}, {time()} \n"
-    print(log)
+    log = f"{key} {time()} \n"
+    add_to_log(log)
 
 def on_release(key):
     if key == Key.esc:
         return False
 
 def count_keys():
-    with open("logfile.txt", "r") as f: 
-        print(f)
+    total_presses = 0
+    with open("logfile.txt", "r") as f:
+        for _ in f.readlines():
+            total_presses += 1
+
+    total_presses = total_presses
+    # print(total_presses)
+
+    starttime_line = linecache.getline("logfile.txt", 1)
+    starttime_list = starttime_line.split()
+    # print(starttime_list)
+    starttime = starttime_list[1]
+
+    endtime_line = linecache.getline("logfile.txt", total_presses)
+    endtime_list = endtime_line.split()
+    # print(endtime_list)
+    endtime = endtime_list[1]
+
+    total_time = float(endtime) - float(starttime)
+
+    cps = total_presses/total_time
+    cpm = cps*60
+
+    print(f"{total_presses} keys in {total_time}. CPM: {cpm}")
+
+    os.remove("logfile.txt")
 
 # with Listener(
 #     on_press = on_press,
 #     on_release = on_release
 # ) as listener:
-#     Listener.join()
+#     listener.join()
 
 listener = Listener(on_press=on_press, on_release=on_release)
 
 
 
 listener.start()
-sleep(5)
+
+sleep(30)
+
 listener.stop()
+
+count_keys()
