191aeaa64c877c73d101a9735029b8537c6503cb
D1STR4CT <slmkuitert@gmail.com>
2021-05-06 18:22:58 +0200

    beta 1.0

--- src/main.py
+++ src/main.py
@@ -1,48 +1,51 @@
 from pynput.keyboard import Key, Listener
 from time import time, sleep
-import os 
 import linecache
 
 
-def add_to_log(log): 
-    with open("logfile.txt", "a") as f:
-        f.write(log)
-
 def on_press(key):
-    log = f"{key} {time()} \n"
-    add_to_log(log)
+    global log
+    log.append(time())
+    return log
 
 def on_release(key):
     if key == Key.esc:
+        global avg
+        print(avg)
         return False
 
-def count_keys():
-    total_presses = 0
-    with open("logfile.txt", "r") as f:
-        for _ in f.readlines():
-            total_presses += 1
+def count_keys(total_log):
+    
+    total_presses = len(total_log)
 
-    total_presses = total_presses
-    # print(total_presses)
-
-    starttime_line = linecache.getline("logfile.txt", 1)
-    starttime_list = starttime_line.split()
-    # print(starttime_list)
-    starttime = starttime_list[1]
-
-    endtime_line = linecache.getline("logfile.txt", total_presses)
-    endtime_list = endtime_line.split()
-    # print(endtime_list)
-    endtime = endtime_list[1]
+    starttime = total_log[1]
+    endtime = total_log[-1]
+    
+    global log 
+    log = []
 
     total_time = float(endtime) - float(starttime)
 
     cps = total_presses/total_time
     cpm = cps*60
 
-    print(f"{total_presses} keys in {total_time}. CPM: {cpm}")
+    print(f"{total_presses} keys in {int(round(total_time, 0))}. CPM: {int(round(cpm, 0))}")
+    global avg
+    avg_new = cpm 
+    avg_old = avg
 
-    os.remove("logfile.txt")
+    if avg == 0:
+        avg = cpm 
+        return avg 
+    else:
+        avg = (avg_new+avg_old)/2
+        print(f"average is: {int(round(avg, 0))}")
+        return avg
+
+
+log = []
+global avg
+avg = 0.0
 
 # with Listener(
 #     on_press = on_press,
@@ -52,12 +55,14 @@ def count_keys():
 
 listener = Listener(on_press=on_press, on_release=on_release)
 
+listener.start() 
 
+while True:
+    loglen = len(log)
+    if loglen > 5:
+        current_time = time()
+        last_log_time = log[-1]
+        if current_time - last_log_time > 2.0:
+            count_keys(log)
+            log = []
 
-listener.start()
-
-sleep(30)
-
-listener.stop()
-
-count_keys()
